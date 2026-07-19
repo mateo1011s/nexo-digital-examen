c1b22caf68a92274c04d45fce3383f83987d4c61
mateo1011s <mateo1011s@users.noreply.github.com>
2026-07-19 14:20:01 +0000

    deploy: 62c42a29fd59baf782bbe048aed2aad7deb061c1

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby16KzRqP0uJC8maVd0tT9JGC8sXojmIQVUtazfHHi05mAuhvrddJKsDr96ZnHWExrl_A/exec',
+  API_TOKEN: 'c27e92f1-1b98-4f2d-a26a-98f49ae4512e'
+};
